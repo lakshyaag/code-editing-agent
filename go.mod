f69f3dd9dce4f90cdf3219e7d565098bc921026c
lakshyaag <lakshyajannu@gmail.com>
2025-07-25 23:38:01 -0400

    add tool calling

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.23.11
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.10.1
+	github.com/invopop/jsonschema v0.13.0
 	github.com/joho/godotenv v1.5.1
 	github.com/openai/openai-go v1.11.1
 )
@@ -14,16 +15,21 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.18.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.11.1 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.4.2 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,27 +3,66 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/invopop/jsonschema"
 	"github.com/joho/godotenv"
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/azure"
 )
 
+type ToolDefinition struct {
+	Name        string                    `json:"name"`
+	Description string                    `json:"description"`
+	InputSchema openai.FunctionParameters `json:"input_schema"`
+	Function    func(input json.RawMessage) (string, error)
+}
+
 type Agent struct {
 	client         *openai.Client
 	model          string
 	getUserMessage func() (string, bool)
+	tools          []ToolDefinition
 }
 
-func NewAgent(client *openai.Client, model string, getUserMessage func() (string, bool)) *Agent {
+var ReadFileDefinition = ToolDefinition{
+	Name:        "read_file",
+	Description: "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+	InputSchema: ReadFileInputSchema,
+	Function:    ReadFile,
+}
+
+type ReadFileInput struct {
+	Path string `json:"path" jsonschema_description:"The relative path of a file in the working directory."`
+}
+
+var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
+
+func ReadFile(input json.RawMessage) (string, error) {
+	readFileInput := ReadFileInput{}
+	err := json.Unmarshal(input, &readFileInput)
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := os.ReadFile(readFileInput.Path)
+	if err != nil {
+		return "", fmt.Errorf("failed to read file %s: %w", readFileInput.Path, err)
+	}
+
+	return string(content), nil
+}
+
+func NewAgent(client *openai.Client, model string, getUserMessage func() (string, bool), tools []ToolDefinition) *Agent {
 	return &Agent{
 		client:         client,
 		model:          model,
 		getUserMessage: getUserMessage,
+		tools:          tools,
 	}
 }
 
@@ -62,7 +101,9 @@ func main() {
 		return scanner.Text(), true
 	}
 
-	agent := NewAgent(&client, model, getUserMessage)
+	tools := []ToolDefinition{ReadFileDefinition}
+
+	agent := NewAgent(&client, model, getUserMessage, tools)
 
 	err = agent.Run(context.TODO())
 
@@ -77,37 +118,116 @@ func (a *Agent) Run(ctx context.Context) error {
 
 	fmt.Printf("Chat with CLI\n")
 
+	readUserInput := true
 	for {
-		fmt.Print("\u001b[94mYou\u001b[0m: ")
-		userInput, ok := a.getUserMessage()
+		if readUserInput {
+			fmt.Print("\u001b[94mYou\u001b[0m: ")
+			userInput, ok := a.getUserMessage()
 
-		if !ok {
-			break
+			if !ok {
+				break
+			}
+
+			userMessage := openai.UserMessage(userInput)
+			conversation = append(conversation, userMessage)
 		}
 
-		userMessage := openai.UserMessage(userInput)
-		conversation = append(conversation, userMessage)
-
-		aiMessage, err := a.runInference(ctx, conversation)
+		message, err := a.runInference(ctx, conversation)
 
 		if err != nil {
 			return err
 		}
 
-		conversation = append(conversation, aiMessage.Choices[0].Message.ToParam())
+		aiMessage := message.Choices[0].Message
 
-		fmt.Printf("\u001b[94mCodeAgent\u001b[0m: %s\n", aiMessage.Choices[0].Message.Content)
+		conversation = append(conversation, aiMessage.ToParam())
 
+		toolResults := []openai.ChatCompletionMessageParamUnion{}
+		if len(aiMessage.ToolCalls) > 0 {
+			for _, toolCall := range aiMessage.ToolCalls {
+				result := a.executeTool(toolCall.ID, toolCall.Function.Name, json.RawMessage([]byte(toolCall.Function.Arguments)))
+
+				toolResults = append(toolResults, result)
+			}
+		} else if len(aiMessage.ToolCalls) == 0 {
+			fmt.Printf("\u001b[94mCodeAgent\u001b[0m: %s\n", aiMessage.Content)
+			readUserInput = true
+			continue
+		}
+
+		readUserInput = false
+		conversation = append(conversation, toolResults...)
 	}
 	return nil
 }
 
 func (a *Agent) runInference(ctx context.Context, conversation []openai.ChatCompletionMessageParamUnion) (*openai.ChatCompletion, error) {
+
+	openaiTools := []openai.ChatCompletionToolParam{}
+	for _, tool := range a.tools {
+		openaiTools = append(openaiTools, openai.ChatCompletionToolParam{
+			Function: openai.FunctionDefinitionParam{
+				Name:        tool.Name,
+				Description: openai.String(tool.Description),
+				Parameters:  tool.InputSchema,
+			},
+		})
+	}
+
 	message, err := a.client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
 		Messages:  conversation,
 		Model:     openai.ChatModel(a.model),
 		MaxTokens: openai.Int(1024),
+		Tools:     openaiTools,
 	})
 
 	return message, err
+}
+
+func (a *Agent) executeTool(id, name string, input json.RawMessage) openai.ChatCompletionMessageParamUnion {
+	var toolDef ToolDefinition
+	var found bool
+
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			toolDef = tool
+			found = true
+			break
+		}
+	}
+	if !found {
+		return openai.ToolMessage(fmt.Sprintf("Tool %s not found", name), id)
+	}
+
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, input)
+
+	result, err := toolDef.Function(input)
+	if err != nil {
+		return openai.ToolMessage(fmt.Sprintf("Error executing tool %s: %v", name, err), id)
+	}
+
+	fmt.Printf("\u001b[92mresult\u001b[0m: %s\n", result)
+	return openai.ToolMessage(result, id)
+}
+
+func GenerateSchema[T any]() openai.FunctionParameters {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: true,
+		DoNotReference:            true,
+	}
+	var v T
+
+	schema := reflector.Reflect(v)
+
+	// Marshal the schema to JSON
+	schemaBytes, err := json.Marshal(schema)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal schema: %v", err))
+	}
+
+	var params openai.FunctionParameters
+	if err := json.Unmarshal(schemaBytes, &params); err != nil {
+		panic(fmt.Sprintf("failed to unmarshal schema to FunctionParameters: %v", err))
+	}
+	return params
 }
